4e8acce9970d5811fa63810b8d924fed25d95d11
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-10-26 19:51:32 -0300

    Manipulando múltiplas requisições em paralelo

--- Practices/Pokedex/assets/js/main.js
+++ Practices/Pokedex/assets/js/main.js
@@ -1,17 +1,20 @@
 const pokemonOl = document.getElementById('pokemonList')
 
+function convertPokemonTypesToLi(pokemonType) {
+    return pokemonType.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join('')}
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+            <img src="${pokemon.sprites.other.dream_world.front_default}" 
             alt="${pokemon.name}" srcset="">
         </div>
     </li>
@@ -20,6 +23,6 @@ function convertPokemonToLi(pokemon) {
 
 pokeApi.getPokemons()
     .then((pokemons = []) => {
-        pokemonOl.innerHTML += pokemons.map(convertPokemonToLi).join('');
+        pokemonOl.innerHTML = pokemons.map(convertPokemonToLi).join('');
     })
     .catch((error) => console.log(error))
