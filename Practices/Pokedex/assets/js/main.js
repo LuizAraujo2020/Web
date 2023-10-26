403e779ec317901320fcccdab869d7379b24970a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-10-26 20:39:38 -0300

    Adicionando os tipos de Pokémon dinamicamente

--- Practices/Pokedex/assets/js/main.js
+++ Practices/Pokedex/assets/js/main.js
@@ -3,13 +3,13 @@ const pokemonOl = document.getElementById('pokemonList')
 function convertPokemonToLi(pokemon) {
     console.log(pokemon)
     return `
-    <li class="pokemon">
+    <li class="pokemon ${pokemon.type}">
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
             </ol>
             <img src="${pokemon.photo}" 
             alt="${pokemon.name}" srcset="">
