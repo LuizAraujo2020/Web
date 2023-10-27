ef001521a2ed9a2b9ad50e85aa29fb771646e300
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-10-26 21:02:07 -0300

    Criando mecanismo de paginação

--- Practices/Pokedex/assets/js/main.js
+++ Practices/Pokedex/assets/js/main.js
@@ -1,25 +1,33 @@
 const pokemonOl = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    console.log(pokemon)
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
+function loadPokemonsItems(offset, limit) {
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" 
-            alt="${pokemon.name}" srcset="">
-        </div>
-    </li>
-    `
+    pokeApi.getPokemons()
+        .then((pokemons = []) => {
+            pokemonOl.innerHTML += pokemons.map((pokemon) => `
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+            
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                        <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}" srcset="">
+                    </div>
+                </li>
+            `).join('');
+        })
+        .catch((error) => console.log(error))
 }
 
-pokeApi.getPokemons()
-    .then((pokemons = []) => {
-        pokemonOl.innerHTML = pokemons.map(convertPokemonToLi).join('');
-    })
-    .catch((error) => console.log(error))
+loadPokemonsItems(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonsItems(offset, limit);
+})
